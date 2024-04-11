c557cfaf136df216ef831243e6e537c037e56c55
Madeleine Eastwood <madeleine@Madeleines-MacBook-Pro.local>
2024-04-11 13:31:39 -0400

    added encode

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ def decode(encoded_pw):
         pw += d
     return pw
 
+def encode(pw):
+    #pw will be an 8-digit string of integers
+    encoded_pw = ''
+    for dig in pw:
+        dig = int(dig)
+        dig += 3
+        dig = str(dig)
+        encoded_pw += dig
+    return encoded_pw
